71880365a3370aff7f18e15f074ec880e82ae6b2
berti <berti@wombless-iMac.local>
2018-07-01 20:49:16 +0100

    rsvp fully working

--- src/containers/WelcomePage/Invitation/Rsvp/Rsvp.js
+++ src/containers/WelcomePage/Invitation/Rsvp/Rsvp.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Aux from '../../../../hoc/Aux/Aux'
+import Spinner from '../../../../components/Spinner/Spinner'
 
 import firebase from '../../../../firebase'
 import ScrollableAnchor, { goToAnchor, goToTop } from 'react-scrollable-anchor'
@@ -13,11 +14,15 @@ class Rsvp extends React.Component {
     textarea = React.createRef()
 
     state = {
-        rsvpState: 'VOTE'
+        rsvpState: 'VOTE',
+        loading: false
     }
 
     vote = (accepted) => {
-        this.setState({ rsvpState: 'COMMENT' })
+        this.setState({ loading: true })
+        firebase.database().ref('guests/' + this.props.userId + '/rsvp').set(
+            accepted ? 'ACCEPTED' : 'DECLINED'
+        ).then(this.setState({ rsvpState: 'COMMENT', loading: false }))
     }
 
     componentDidUpdate() {
@@ -26,12 +31,18 @@ class Rsvp extends React.Component {
         }
     }
 
-    comment = () => {
-        this.setState({ rsvpState: 'COMMENTED' })
+    comment = (event, val) => {
+        this.setState({ loading: true })
+        firebase.database().ref('guests/' + this.props.userId + '/comment').set(
+            this.textarea.current.value
+        ).then(this.setState({ rsvpState: 'COMMENTED', loading: false }))
     }
 
     render() {
         console.log("Rendering RSVP")
+        if (this.state.loading) {
+            return <Spinner />
+        }
         if (this.props.rsvp === 'ACCEPTED') {
             return (<p> Great that you think you can make it. More details to come soon. </p>)
         } else if (this.props.rsvp === 'DECLINED') {
@@ -44,10 +55,10 @@ class Rsvp extends React.Component {
                         <p> We know it's early days, but by choosing one of the following options you can help us with our plans</p>
                         <div className='button-group'>
                             <div className='button-col'>
-                                {<button onClick={this.vote.bind(true)} className='btn btn-light btn-block'>Great! Looking forward to it</button>}
+                                {<button onClick={() => this.vote(true)} className='btn btn-light btn-block'>Great! Looking forward to it</button>}
                             </div>
                             <div className='button-col'>
-                                <button onClick={this.vote.bind(false)} className='btn btn-light btn-block'>Sorry, {this.props.names && this.props.names.length === 1 ? 'I' : 'we'} can't make it</button>
+                                <button onClick={() => this.vote(false)} className='btn btn-light btn-block'>Sorry, {this.props.names && this.props.names.length === 1 ? 'I' : 'we'} can't make it</button>
                             </div>
                         </div>
                     </Aux>
@@ -57,8 +68,7 @@ class Rsvp extends React.Component {
                     <Aux>
                         <ScrollableAnchor id={'a-comment'}><div /></ScrollableAnchor>
                         <div className='row animated fadeIn'>
-                            <p>Thanks for letting us know. We'll be in touch soon with more info.</p>
-                            <p>Feel free to leave us a comment:</p>
+                            <p>Thanks for letting us know. Feel free to leave us a comment:</p>
                             <div id='comment-box'>
                                 <textarea ref={this.textarea} />
                                 <div className='comment-btn'>
