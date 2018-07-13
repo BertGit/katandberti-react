7d22d1d255c21da40562d528561108de0f6de185
berti <berti@wombless-iMac.local>
2018-07-13 09:43:31 +0100

    First draft: Localization

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+import LocalizedStrings from 'react-localization'
+
 import './Toolbar.css'
 import '../../css/Buttons.css'
 import logo from '../../assets/images/logoKB.png'
 
+let strings = new LocalizedStrings({
+    en: {
+        rsvp: "r.s.v.p"
+    }, de: {
+        rsvp: "Rückmeldung"
+    }
+})
+
 class Toolbar extends React.PureComponent {
+
     toolbarRef = React.createRef();
 
     componentDidMount = () => {
@@ -17,7 +28,7 @@ class Toolbar extends React.PureComponent {
                 <img src={logo} className='logo' alt='' />
                 {this.props.showRsvp ?
                     <a href='#a-rsvp'>
-                        <button className='btn btn-dark btn-rsvp'>r.s.v.p</button>
+                        <button className='btn btn-dark btn-rsvp'>{strings.rsvp}</button>
                     </a>
                     : null}
             </nav>
