b9985559969257a34057b69e778662e6c1c7c270
tsiq-bertram <bertram.mueller@twosigmaiq.com>
2019-02-24 20:22:39 +0000

    Updated questions

--- src/containers/WelcomePage/Invitation/EventDetails/FAQs/FAQs.js
+++ src/containers/WelcomePage/Invitation/EventDetails/FAQs/FAQs.js
@@ -20,17 +20,17 @@ export default class Accordion extends React.PureComponent {
                     Women – Hats/Fascinators always welcome<br />
                     We kindly ask please no jeans<br />
                 </p></Aux>)}
-                {this.accordion("What's the weather like?", <Aux><p>
+                {this.accordion("What will the weather be like?", <Aux><p>
                     It can be hit and miss this time of year. However, it is the south of France so generally a bit warmer climate than what we are used to in June!
                     Remember to bring your umbrella and also some warmer clothes for later in the evenings. </p></Aux>)}
                 {this.accordion("Can I bring plus ones?", <Aux><p>We unfortunately cannot accommodate more people other than those we have personally invited, so we kindly ask that guests do not bring a plus one. </p></Aux>)}
-                {this.accordion("How is Brexit affecting me?", <Aux><p>As the laws of travel to EU may change after 29 March in the event of no–deal, please check <b>your passports have more than 15 months validity left</b> on them before you travel.
+                {this.accordion("Will Brexit affect my travel plans?", <Aux><p>As the laws of travel to EU may change after 29 March in the event of no–deal, please check <b>your passports have more than 15 months validity left</b> on them before you travel.
 </p><p>
                         You can check the validity of your passport here <a href="https://www.passport.service.gov.uk/check-a-passport" target="blank">www.passport.service.gov.uk/check-a-passport</a>
                     </p><p>
                         There are no changes to the law regarding visas until 2020, so only the validity of your passport is important to check.
 </p></Aux>)}
-                {this.accordion("Arriving by car? What's the law?", <Aux><p>France has some stricter driving laws than the UK, so if you are driving your own car over to France we advise you to look at the checklist to make sure you have the things in your car required by French law.
+                {this.accordion("I'm planning to travel by car, anything I should know?", <Aux><p>France has some stricter driving laws than the UK, so if you are driving your own car over to France we advise you to look at the checklist to make sure you have the things in your car required by French law.
 </p><p>
                         Find out more in the link below <br /><a href="https://www.rac.co.uk/drive/travel/country/france/" target="blank">www.rac.co.uk/drive/travel/country/france/</a>
                     </p><p>
@@ -40,7 +40,7 @@ export default class Accordion extends React.PureComponent {
 </p><p>
                         For those wishing to drive straight to the Church, please note that there is very little parking available in the tiny village of Limueil. To avoid complications with parking we recommend you take our coach – but you are welcome to make your own way there if you prefer.
 </p></Aux>)}
-                {this.accordion("If I don't stay at the venue, how will I get back home after the celebration night?", <Aux><p>We will be organising a shuttle service to take guests within a maximum of a 30-minute drive back to their accommodations after the party has finished. If you would like to use this service, we kindly ask that guests contribute and bring euros to help towards the cost of this.
+                {this.accordion("How will I get back to my accommodation after the party has finished?", <Aux><p>We will be organising a shuttle service to take guests within a maximum of a 30-minute drive back to their accommodations after the party has finished. If you would like to use this service, we kindly ask that guests contribute and bring euros to help towards the cost of this.
 </p><p>All guests with cars can drive to our venue Les Magnolias and park outside the Intermarche which is opposite the venue and has a big car park - this is free parking. If you plan to drive to our venue in the morning and prefer to take a taxi back in the evening after the party, you can leave your car at the Intermarche overnight.
 </p></Aux>)}
             </div>
